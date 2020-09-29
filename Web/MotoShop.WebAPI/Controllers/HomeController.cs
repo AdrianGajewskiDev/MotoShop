295d58fdb1e2f2fac891602d3a6401cd7fa4525d
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2020-09-29 12:57:26 +0200

    Renamed Web project to WebAPI, add Data layer project

---
+++ Web/MotoShop.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MotoShop.Web.Controllers
+{
+    [ApiController]
+    [Route(template: "api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("Home index value");
+        }
+    }
+}
