89cc700b597443a35d4e418ef742598e5f47caa8
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2020-10-11 16:03:56 +0200

    add and style navbar component

--- Client/MotoShop-Client/src/app/app.module.ts
+++ Client/MotoShop-Client/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {AngularMaterialModule} from "./modules/angular.material.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
+    AngularMaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Client/MotoShop-Client/src/app/modules/angular.material.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from "@angular/core"
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule(
+{
+    declarations:[],
+    imports:[],
+    exports:[
+    MatIconModule
+    ]
+})
+
+export class AngularMaterialModule { }
