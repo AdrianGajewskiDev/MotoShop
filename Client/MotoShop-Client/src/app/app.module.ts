775d9aff016c2b121a5e273391117087820b156a
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2020-10-29 20:38:52 +0100

    add and style footer

--- Client/MotoShop-Client/src/app/app.module.ts
+++ Client/MotoShop-Client/src/app/app.module.ts
@@ -7,11 +7,13 @@ import {FlexLayoutModule} from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     AngularMaterialModule,
