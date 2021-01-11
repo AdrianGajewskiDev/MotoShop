968613bc17e008ea31866811cb6fb3a233894798
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2021-01-11 17:11:05 +0100

    add base layout to administration panel component

--- Client/MotoShop-Client/src/app/administration-panel/administration-panel.component.ts
+++ Client/MotoShop-Client/src/app/administration-panel/administration-panel.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { slideInOutAnimation } from '../shared/animations';
 
 @Component({
   selector: 'app-administration-panel',
   templateUrl: './administration-panel.component.html',
-  styles: []
+  styleUrls: ["administration.component.sass"],
+  animations: [slideInOutAnimation]
 })
 export class AdministrationPanelComponent implements OnInit {
+
+  public animationState: "slideIn" | "slideOut" = "slideIn";
 
   constructor() { }
 
