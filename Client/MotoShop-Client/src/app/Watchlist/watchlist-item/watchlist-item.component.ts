32cfd03715a8174707bd1a7b1e0b39ac5a174c81
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2021-04-18 19:00:37 +0200

    add animations to WatchlistComponent pagfe

--- Client/MotoShop-Client/src/app/Watchlist/watchlist-item/watchlist-item.component.ts
+++ Client/MotoShop-Client/src/app/Watchlist/watchlist-item/watchlist-item.component.ts
@@ -1,7 +1,8 @@
 import { DatePipe } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { removeWatchlistItemAnimation } from 'src/app/shared/animations';
 import { buildImagePath } from 'src/app/shared/Helpers/buildProfileImagePath';
 import { WatchListItemModel } from 'src/app/shared/models/Watchlist/watchlistItemModel';
 import { WatchlistService } from 'src/app/shared/services/watchlist.service';
@@ -9,7 +10,8 @@ import { WatchlistService } from 'src/app/shared/services/watchlist.service';
 @Component({
   selector: 'app-watchlist-item',
   templateUrl: './watchlist-item.component.html',
-  styleUrls: ['./watchlist-item.component.sass']
+  styleUrls: ['./watchlist-item.component.sass'],
+  animations: [removeWatchlistItemAnimation]
 })
 export class WatchlistItemComponent implements OnInit {
 
@@ -19,20 +21,28 @@ export class WatchlistItemComponent implements OnInit {
   public slides: string[] = [];
 
   public btnMessage = "Unwatch";
+  public animationState: "default" | "activate" = "default";
+
 
   public pinDateTransformed;
   constructor(private datePipe: DatePipe,
     private router: Router,
     private service: WatchlistService,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private crDocument: ElementRef<HTMLDocument>) { }
 
   ngOnInit(): void {
+
     for (let url of this.Model.ImageUrls)
       this.slides.push(this.buildImageUrl(this.baseServrResourcesPath + url));
 
     this.pinDateTransformed = this.datePipe.transform(this.Model.PinDate, "dd-mm-yyyy")
   }
 
+  hideElement(e) {
+    if (this.animationState == "activate")
+      (this.crDocument.nativeElement.querySelector(".item-container") as HTMLElement).style.display = "none";
+  }
 
   buildImageUrl(url) {
     return buildImagePath(url);
@@ -44,8 +54,11 @@ export class WatchlistItemComponent implements OnInit {
 
   deleteItem() {
     this.btnMessage = "Processing..."
+    this.animationState = "activate";
+
     this.service.deleteWatchlistItem(this.Model.Id).subscribe(() => {
       this.toastr.success("Successfully deleted from your watchlist")
+      this.animationState = "activate";
       this.btnMessage = "Done";
     },
       error => console.log(error));
--- Client/MotoShop-Client/src/app/shared/animations.ts
+++ Client/MotoShop-Client/src/app/shared/animations.ts
@@ -1,12 +1,20 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 export const slideInOutAnimation = trigger('slideInOut', [
-      state("slideIn", style({
-        transform: 'translateX(0)' 
-      })),
-      state("slideOut", style({
-        transform: 'translateX(-300%)' 
-      })),
-      transition("* => slideIn", animate("0.3s")),
-      transition("* => slideOut", animate("0.3s")),
+  state("slideIn", style({
+    transform: 'translateX(0)'
+  })),
+  state("slideOut", style({
+    transform: 'translateX(-300%)'
+  })),
+  transition("* => slideIn", animate("0.3s")),
+  transition("* => slideOut", animate("0.3s")),
+]);
+
+export const removeWatchlistItemAnimation = trigger("removeWatchlistItemAnimation", [
+  transition("* => activate", animate("0.7s", keyframes([
+    style({ transform: 'translateY(0)', opacity: 1, offset: 0 }),
+    style({ transform: 'translateY(-10%)', opacity: 0.8, offset: 0.1 }),
+    style({ transform: 'translateY(300%)', opacity: 0, offset: 1 }),
+  ])))
 ]);
